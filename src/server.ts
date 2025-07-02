90b00c9b7b30f683c530a9b2280d10bbd6c4812a
Joel Riba <joel.irungu00@gmail.com>
2025-07-02 18:13:24 +0300

    v1

--- src/server.ts
+++ src/server.ts
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(
 
     cors({
-        origin: ['http://localhost:4200', 'https://riba-tech-project.vercel.app'], 
+        origin: '*', 
         credentials: true,
         methods: ['GET', 'POST', 'PUT', 'DELETE'],
         allowedHeaders: ['Content-Type', 'Authorization'],
